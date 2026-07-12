040728615023d6ca8fff2970442fc45588fc1715
taimoor5356 <mtaimoornaveed@gmail.com>
2026-07-12 09:19:06 +0500

    updated

--- resources/js/components/NovaStore/HeroBanner.vue
+++ resources/js/components/NovaStore/HeroBanner.vue
@@ -24,7 +24,7 @@
                     <img
                         :src="banner.thumbnail"
                         loading="lazy"
-                        class="w-full object-contain rounded-lg"
+                        class="w-full object-contain rounded-lg lg:h-[350px] lg:object-cover"
                     />
                 </swiper-slide>
             </swiper>
